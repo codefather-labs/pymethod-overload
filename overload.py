5262a6f45eb2c398650aeccfce99a6d5c228e790
codefather <codefather.ru@gmail.com>
2022-08-04 22:10:09 +0300

    need default value fix

--- overload.py
+++ overload.py
@@ -1,16 +1,22 @@
 import inspect
-from functools import lru_cache
 from typing import Callable, Optional
 
-default_hash_table = dict()
+table = dict()
+
+
+def debug_message(message: str):
+    print(f"[overload-debug]: {message}")
 
 
 class Overload:
-    def __init__(self, separate_hash_table: dict = None):
+    __DEFAULT_HASH_TABLE = dict()
+
+    def __init__(self, separate_hash_table: dict = None, debug: bool = False):
         self.func: Optional[Callable] = None
         self.hash_table = separate_hash_table \
-            if separate_hash_table else default_hash_table
+            if separate_hash_table else table
         self.argspec_key_pattern = "{args}:{defaults}:{returns}"
+        self.debug = debug if debug else False
 
     def get_function_hash(self, key):
         # Default hash function algo
@@ -31,11 +37,27 @@ class Overload:
         """
         self.func = args[0]
 
-        # DEFINITION CASE
+        # Definition case
         # Inspecting wrapped function for getting types annotations
         argspec: inspect.FullArgSpec = inspect.getfullargspec(self.func)
 
-        # COMMON CASE
+        # TODO:
+        #   add default value support in cases
+        #   when it was declared and func
+        #   ---
+        #   >>> @overload
+        #   >>> def fetch(self, a: Optional[List] = None) -> List:
+        #   >>>     return list()
+        #   ---
+        #   but not given at `fetch` call:
+        #       fetch() > TypeError: TestArrays.fetch()
+        #       missing 1 required positional argument: 'a'
+        #   bug passed if we call fetch with all arguments
+        #       fetch(None) > works
+        #   ---
+        #   try python3 trouble.py
+
+        # Ccomon case
         # Declaring key for hash table (it's not unique yet)
         key = self.argspec_key_pattern.format(
             args=argspec.args,
@@ -43,17 +65,17 @@ class Overload:
             returns=argspec.annotations.get('return')
         )
 
-        # COMMON CASE
+        # Common case
         # Hash function algo
         function_hash_key = self.get_function_hash(key)
 
-        # DEFINITION CASE
+        # Definition case
         # Saving function in hash table for hash_function_key
         self.hash_table.update({
             function_hash_key: self.func
         })
 
-        # EVALUATION CASE
+        # Evaluation case
         # Return a wrapper that will return the
         # target function from the hash table
         def call_function_by_hash_table_key(*a, **kw):
@@ -64,5 +86,8 @@ class Overload:
                 # was not found in hash table
                 # In this case will be returns input wrapped func
                 return self.func(*a, **kw)
+
+        if self.debug:
+            debug_message(f"hash table state: {self.hash_table}")
 
         return call_function_by_hash_table_key
---
+++ trouble.py
@@ -0,0 +1,32 @@
+from typing import Optional, List, Tuple
+
+from overload import Overload
+
+overload = Overload(debug=True)
+
+
+class TestArrays:
+    @overload
+    def fetch(self, a: Optional[List] = None) -> List:
+        return list()
+
+    @overload
+    def fetch(self, a: List) -> List:
+        return list()
+
+    @overload
+    def fetch(self, a: Tuple) -> Tuple:
+        return tuple()
+
+
+t = TestArrays()
+# TODO t.fetch(None) works
+print(t.fetch(None))
+# TODO t.fetch(None) TypeError: TestArrays.fetch()
+#       missing 1 required positional argument: 'a'
+#       bug details inside of overload.__call__
+print(t.fetch())
+
+
+print(t.fetch(list()))
+print(t.fetch(tuple()))
